01b1dc36040285032ca50c048f6ba33b44ece86d
RopE17 <81800443+RopE17@users.noreply.github.com>
2021-08-27 13:57:01 +0300

    Create banlist.js

---
+++ komutlar/banlist.js
@@ -0,0 +1,13 @@
+module.exports = ({
+  name:'banlist',
+  code:`$color[aafcbc]
+  $description[
+Bu sunucuda toplam \`$banCount\` banlı üye var.
+
+ >>> **▬▬▬▬▬▬▬▬DMX▬▬▬▬▬▬▬▬▬▬**
+\`\`\`fix
+$replaceText[$usersBanned;,;
+;-1]\`\`\`
+ **▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬**]
+`
+})
